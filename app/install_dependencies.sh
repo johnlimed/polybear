e341064d5947636ebcfe63cffd6c2ad62d0545f0
John Lim <johnlimed@gmail.com>
2017-03-18 01:50:14 +0800

    Added the NVM

--- app/install_dependencies.sh
+++ app/install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# node version management
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+nvm install node
+
 # install rethinkDB
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
