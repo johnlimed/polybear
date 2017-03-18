267838c44337d22ac17f4e1e80fda2df9d8175f4
John Lim <johnlimed@gmail.com>
2017-03-18 23:57:34 +0800

    send new nginx config

--- app/install_dependencies.sh
+++ app/install_dependencies.sh
@@ -4,6 +4,9 @@
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 nvm install node
 
+# install nginx
+sudo apt-get install nginx
+
 # install rethinkDB
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
--- app/send_items.sh
+++ app/send_items.sh
@@ -13,6 +13,8 @@ COMODORSA='ssl/COMODORSADomainValidationSecureServerCA.crt'
 SERVER='ubuntu@54.255.144.173'
 CONFIGTARGETDIR=':~/polybear/app/server/config'
 SSLTARGETDIR=':~/polybear/app/ssl'
+NGINXCONFIG='server/config/dashy.sg'
+NGINXTARGETDIR=':~/polybear/app/server/config'
 
 # scp config files to server
 scp $CONFIGDIR$TELECONFIG $SERVER$CONFIGTARGETDIR
@@ -23,3 +25,4 @@ scp $COMODOCA $SERVER$SSLTARGETDIR
 scp $ADDTRUSTCA $SERVER$SSLTARGETDIR
 scp $COMODORSA $SERVER$SSLTARGETDIR
 scp $BUNDLECERT $SERVER$SSLTARGETDIR
+scp $NGINXCONFIG $SERVER$NGINXTARGETDIR
